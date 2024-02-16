b36d72d89f4132d37321f5a1e133e96f5d50bdf7
Aldo Munaretto <aldo.munaretto@aggity.com>
2024-02-16 12:13:12 +0000

    Added documentation on deployment scripts

--- scripts/MarketPlace.deploy.js
+++ scripts/MarketPlace.deploy.js
@@ -1,19 +1,27 @@
 const hre = require("hardhat");
 
+// Variables globales para almacenar la instancia del contrato desplegado y la dirección del contrato.
 let deployedContract
 let contractAddress
 
+// Despliegue del contrato MyMarketPlace y espera a que se complete el despliegue.
+// Requiere como parámetros las direcciones de los contratos MyCoin y MyNFTCollection.
 async function deploy(CoinContractAddress,NFTContractAddress){
     console.log("MarketPlace deployment has just started...")
+    // Obtenemos el contrato MyMarketPlace.
     const MarketPlaceContract = await ethers.getContractFactory("MyMarketPlace")
+    // Desplegamos el contrato utilizando como parámetros las direcciones de los contratos MyCoin y MyNFTCollection.
     deployedContract = await MarketPlaceContract.deploy(CoinContractAddress,NFTContractAddress)
+    // Esperamos a que se complete el despliegue.
     await deployedContract.waitForDeployment()
+    // Obtenemos la dirección del contrato desplegado.
     contractAddress = deployedContract.target
     console.log("...MarketPlace contract has been deployed to: " + contractAddress)
 }
 
 async function verify(){}
 
+//Función para obtener la dirección del contrato desplegado.
 async function getContractAddress(){
     return contractAddress
 }
