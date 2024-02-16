261cca32b8ca0ab9a31b0f4f74787742627192bb
Aldo Munaretto <aldo.munaretto@aggity.com>
2024-02-16 15:11:39 +0000

    Added documentation son smart contracts

--- scripts/deployment_manager.js
+++ scripts/deployment_manager.js
@@ -1,6 +1,6 @@
 // Para desplegar: npx hardhat run scripts/deployment_manager.js --network localhost
 
-// Obtiene los scripts de despliegue de los contratos ERC20, ERC721 y MarketPlace.
+// Obtiene los scripts de despliegue de los contratos MyCoin, MyNFTCollection y MyMarketPlace.
 const ERC20DeployScript = require("./ERC20.deploy")
 const ERC721DeployScript = require("./ERC721.deploy")
 const MarketPlaceDeployScript = require("./MarketPlace.deploy")
@@ -15,7 +15,7 @@ const main = async () => {
     await ERC721DeployScript.deploy()
     // Obtiene la dirección del contrato MyNFTCollection.
     let MyNFTCollectionContractAddress = await ERC721DeployScript.getContractAddress()
-    // Despliega el contrato MarketPlace utilizando las direcciones de los contratos de MyCoin y MyNFTCollection.
+    // Despliega el contrato MyMarketPlace utilizando las direcciones de los contratos de MyCoin y MyNFTCollection.
     await MarketPlaceDeployScript.deploy(MyCoinContractAddress, MyNFTCollectionContractAddress)
 }
 
